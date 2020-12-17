11c85044e66d2f167a53eebb99ff21c17a6a434a
DeyberLuna <70173528+DeyberLuna@users.noreply.github.com>
2020-12-16 23:15:23 -0500

    Archivo django eliminado

--- django/TrainningApp/warehouse/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Category(models.Model) :
-    code = models.CharField(max_length=20)
-    name = models.CharField(max_length=100)
-    status = models.BooleanField(default=True)
-    create_date = models.DateTimeField('Date creation')
-    update_date = models.DateTimeField('Date update')
