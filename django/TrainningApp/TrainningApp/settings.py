a9684d54b2a62c7724885ddaa6df25947189e0d9
DeyberLuna <70173528+DeyberLuna@users.noreply.github.com>
2020-12-16 22:21:12 -0500

    Reporte trainninApp

--- django/TrainningApp/TrainningApp/settings.py
+++ django/TrainningApp/TrainningApp/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+from decouple import config
+import dj_database_url
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,14 +25,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '8fghq8(vkg36_m+aju9=i5qfl=4nunq7q=^qjfsgjbc!h8ttpz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'market.apps.MarketConfig',
+    'warehouse.apps.WarehouseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,11 +78,48 @@ WSGI_APPLICATION = 'TrainningApp.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        
+        'ENGINE'   : 'django.db.backends.postgresql_psycopg2',
+        'NAME'     : 'trainingApp',
+        'USER'     : 'postgres',
+        'PASSWORD' : 'deyber7777',
+        'HOST'     : 'localhost',
+        'PORT'     : '5432',
+    },
+    'backup' : {
+        'ENGINE' : 'django.db.backends.sqlite3',
+        'NAME'   :  BASE_DIR / 'db.sqlite3',
+    },
+
+    'warehouse': {
+        
+        'ENGINE'   : 'django.db.backends.postgresql_psycopg2',
+        'NAME'     : 'warehouse',
+        'USER'     : 'postgres',
+        'PASSWORD' : 'deyber7777',
+        'HOST'     : 'localhost',
+        'PORT'     : '5432',
+    },
+    
+    'market': {
+        
+        'ENGINE'   : 'django.db.backends.postgresql_psycopg2',
+        'NAME'     : 'market',
+        'USER'     : 'postgres',
+        'PASSWORD' : 'deyber7777',
+        'HOST'     : 'localhost',
+        'PORT'     : '5432',
     }
 }
+
+DATABASE_ROUTERS= [
+    'market.routers.MarketRouter',
+    'warehouse.routers.WareHouseRouter',
+    
+    
+]
 
 
 # Password validation
@@ -118,3 +159,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIR =  [os.path.join(BASE_DIR, 'static')]
+
+
